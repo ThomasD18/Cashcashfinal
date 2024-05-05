8e7acc11412a333cf6a922fb09402d2e6219546e
ThomasD18 <104076632+ThomasD18@users.noreply.github.com>
2024-05-05 13:12:20 +0200

    Add files via upload

---
+++ cashcash/Acceuil.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+        h1 {
+            text-align: center;
+            color: #333;
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+        .button-container {
+            display: flex;
+            justify-content: center;
+            margin-top: 50px;
+        }
+        .button {
+            margin: 0 10px;
+        }
+        a {
+            display: inline-block;
+            padding: 15px 30px;
+            background-color: #007bff;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+            font-size: 16px;
+            transition: background-color 0.3s ease;
+        }
+        a:hover {
+            background-color: #0056b3;
+        }
+    </style>
+    <title>Accueil</title>
+</head>
+<body>
+
+    <h1>Choisissez une option :</h1>
+
+    <div class="button-container">
+        <a class="button" href="/Cashcash2/cashcash/index.php">Assistant</a>
+        <a class="button" href="/Cashcash2/cashcash/Technicien/Romain.php">Technicien</a>
+    </div>
+
+</body>
+</html>
